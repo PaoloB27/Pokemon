2d7a26ba6b57da0bf1bc44c6219b89acb357f67e
Paolo B. PhD <paolo.bresolin.2@phd.unipd.it>
2025-07-15 12:59:27 +0200

    Simulation completed

--- assignment_5/src/simulations.py
+++ assignment_5/src/simulations.py
@@ -249,10 +249,10 @@ def parse_args():
     # arguments
     parser.add_argument("--n_games", type=int, required=False, default=1000, help="Number of games to run.")
     parser.add_argument("--n_battles", type=int, required=False, default=500, help="Number of battles to run in each game.")
-    parser.add_argument("--input_pokemons", type=str, required=False, default=os.path.join("..", "data", "pokemons.json"), help="Path to the dataset with pokemons.")
-    parser.add_argument("--input_moves", type=str, required=False, default=os.path.join("..", "data", "moves.json"), help="Path to the dataset with moves.")
-    parser.add_argument("--input_type_effectiveness", type=str, required=False, default=os.path.join("..", "data", "type_effectiveness.json"), help="Path to the dataset with type effectiveness pairs.")
-    parser.add_argument("--output_data", type=str, required=False, default=os.path.join("simulation_data", "collected_data.csv"), help="Path to the file where to save the collected data.")
+    parser.add_argument("--input_pokemons", type=str, required=False, default=os.path.join("..", "..", "data", "pokemons.json"), help="Path to the dataset with pokemons.")
+    parser.add_argument("--input_moves", type=str, required=False, default=os.path.join("..", "..", "data", "moves.json"), help="Path to the dataset with moves.")
+    parser.add_argument("--input_type_effectiveness", type=str, required=False, default=os.path.join("..", "..", "data", "type_effectiveness.json"), help="Path to the dataset with type effectiveness pairs.")
+    parser.add_argument("--output_data", type=str, required=False, default=os.path.join("..", "simulation_data", "collected_data.csv"), help="Path to the file where to save the collected data.")
     parser.add_argument("--random_seed", type=int, required=False, default=27, help="Random seed for reproducibility.")
                           
     return parser.parse_args()
